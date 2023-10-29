b1c2563f79254bd17bbdf62287b021677875dc62
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2023-10-29 08:34:34 +0700

    Initial commit

---
+++ SK-Lib-stdlib.h-labs-Bagian4__C/Source.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+/*
+	Source by AlphaCodingSkills (https://www.alphacodingskills.com)
+	Modified For Learn by RK
+	I.D.E : VS2022
+*/
+
+int main() {
+	printf("labs(10) = %ld\n", labs(10));
+	printf("labs(-10) = %ld\n", labs(-10));
+	printf("labs(50) = %ld\n", labs(-50));
+	printf("labs(-50) = %ld\n", labs(50));
+
+	_getch();
+	return 0;
+}
